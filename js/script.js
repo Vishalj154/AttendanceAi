32417c833779c7dd62000b00dc17e5d38b9e055d
Vishal <vishaljankar2534@gmail.com>
2026-03-14 12:16:41 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// smooth scroll
+
+document.querySelectorAll("a").forEach(link=>{
+
+link.addEventListener("click",function(e){
+
+if(this.hash !== ""){
+
+e.preventDefault();
+
+const target=document.querySelector(this.hash);
+
+target.scrollIntoView({
+behavior:"smooth"
+})
+
+}
+
+})
+
+})
